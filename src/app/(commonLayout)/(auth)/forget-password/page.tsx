e15e9c03a72c1299d7b85b9acfee6597809fa2cd
rudra0700 <baruarudra7@gmail.com>
2026-06-24 17:59:41 +0600

    part-2 added

---
+++ src/app/(commonLayout)/(auth)/forget-password/page.tsx
@@ -0,0 +1,12 @@
+
+
+const ForgetPasswordPage = () => {
+    return (
+        <div>
+            <h1>Forget Password page</h1>
+
+        </div>
+    );
+};
+
+export default ForgetPasswordPage;
---
+++ src/app/(commonLayout)/doctors/[id]/page.tsx
@@ -0,0 +1,11 @@
+
+
+const DynamicDoctorPage = () => {
+    return (
+        <div>
+            <h1>Doctor Profile : 1</h1>
+        </div>
+    );
+};
+
+export default DynamicDoctorPage;
